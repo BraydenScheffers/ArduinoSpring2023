1e81befac482747ce707b7e50251a3e6d2eab50f
Brayden Scheffers <02brayden.scheffers@gulllakecs.org>
2023-03-06 13:53:26 -0500

    Added Pin 10 (3rd LED) and changed Timing
    Stable

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -31,22 +31,34 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
-
+  pinMode(10, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(12, HIGH);   // turn pin 12 to HIGH so LED turns on
   digitalWrite(11, HIGH);   // turn pin 11 to HIGH so LED turns on
+  digitalWrite(10, HIGH);   // turn pin 10 to HIGH so LED turns on
   delay(1000);                       // wait for a second
   digitalWrite(12, LOW);    // turn pin 12 to LOW so LED turns off
   digitalWrite(11, LOW);    // turn pin 11 to LOW so LED turns off
+  digitalWrite(10, LOW);    // turn pin 10 to LOW so LED turns off
   delay(1000);                       // wait for a second
 
 digitalWrite(12, HIGH);   // turn pin 12 to HIGH so LED turns on
-  delay(3000);                       // wait for 3 seconds
+  delay(1000);                       // wait for 1 second
   digitalWrite(12, LOW);    // turn pin 12 to LOW so LED turns off
-  delay(3000);                       // wait for 3 seconds
+  delay(1000);                       // wait for 1 second
 
+digitalWrite(11, HIGH);   // turn pin 11 to HIGH so LED turns on
+  delay(1000);                       // wait for 1 second
+  digitalWrite(11, LOW);    // turn pin 11 to LOW so LED turns off
+  delay(1000);                       // wait for 1 second
+
+digitalWrite(10, HIGH);   // turn pin 10 to HIGH so LED turns on
+  delay(1000);                       // wait for 1 second
+  digitalWrite(10, LOW);    // turn pin 10 to LOW so LED turns off
+  delay(1000);                       // wait for 1 second
+ 
  
 }
