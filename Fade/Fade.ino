97080871734f9c3c32fd1923bf5f4aa1103f0f3c
Brayden Scheffers <02brayden.scheffers@gulllakecs.org>
2023-03-14 14:05:47 -0400

    Added Pin 10 Fading -Stable

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -17,7 +17,7 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
 */
 
-int led = 9;           // the PWM pin the LED is attached to
+int led = 10;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
@@ -29,7 +29,7 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
+  // set the brightness of pin 10:
   analogWrite(led, brightness);
 
   // change the brightness for next time through the loop:
