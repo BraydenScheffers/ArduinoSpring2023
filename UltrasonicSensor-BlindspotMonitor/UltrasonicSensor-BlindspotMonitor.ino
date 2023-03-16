29e91256c1dee456fe441fb5dede92ee708373a1
Brayden Scheffers <02brayden.scheffers@gulllakecs.org>
2023-03-16 14:09:36 -0400

    Initial UltrasonicSensor-BlindspotMonitor Commit
    Stable

---
+++ UltrasonicSensor-BlindspotMonitor/UltrasonicSensor-BlindspotMonitor.ino
@@ -0,0 +1,18 @@
+//Programmer: Brayden Scheffers
+//Date: 16.3.2023
+//Program: UltrasonicSensor-BlindspotMonitor
+
+/*
+ * This program will determine distances of approaching veichles and that are in your blind spot. 
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
