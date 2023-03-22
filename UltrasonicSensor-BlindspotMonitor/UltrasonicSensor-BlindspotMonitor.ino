d3416675cdd9d987feab305bbea430e23eaa30b2
Brayden Scheffers <02brayden.scheffers@gulllakecs.org>
2023-03-22 13:45:23 -0400

    Copy /Paste from Resource

--- UltrasonicSensor-BlindspotMonitor/UltrasonicSensor-BlindspotMonitor.ino
+++ UltrasonicSensor-BlindspotMonitor/UltrasonicSensor-BlindspotMonitor.ino
@@ -1,11 +1,11 @@
 //Programmer: Brayden Scheffers
 //Date: 16.3.2023
 //Program: UltrasonicSensor-BlindspotMonitor
+//Resource: https://projecthub.arduino.cc/Krepak/a6ea3ad5-fd23-49c9-a96c-5e6fcf97f2cc?ref=search&ref_id=red%20green%20yellow%20light%20echo%20sensor&offset=1
 
 /*
  * This program will determine distances of approaching veichles and that are in your blind spot. 
  */
-
 
 void setup() {
   // put your setup code here, to run once:
