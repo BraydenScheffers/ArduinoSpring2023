eb6f551a8f651e39a6484992ede6641ec36db37b
Brayden Scheffers <02brayden.scheffers@gulllakecs.org>
2023-04-05 13:46:25 -0400

    WIP

--- UltrasonicSensor-BlindspotMonitor/ReadAnalogVoltage/ReadAnalogVoltage.ino
+++ UltrasonicSensor-BlindspotMonitor/ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -23,58 +23,42 @@ int LEDlampGreen = 9;
 int LEDlampBlue = 10; 
 int LEDlampRed = 11;  
 int soundbuzzer = 3; 
-int sound  = 500;
+int sound  = 100;
 
 
 void setup() {
   Serial.begin (9600);
   pinMode(trigPin,  OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(LEDlampGreen, OUTPUT);
-  pinMode(LEDlampBlue,  OUTPUT);
   pinMode(LEDlampRed, OUTPUT);
+  pinMode(LEDlampBlue,  OUTPUT);
+  pinMode(LEDlampGreen, OUTPUT);
   pinMode(soundbuzzer, OUTPUT);
 }
 void  loop() {
-  long durationindigit, distanceincm;
+  long durationindigit, distanceinft;
   digitalWrite(trigPin, LOW);  
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   durationindigit = pulseIn(echoPin, HIGH);
-  distanceincm = (durationindigit/5) / 29.1;
- 
-  if (distanceincm < 50) {
-      digitalWrite(LEDlampGreen, HIGH);
-}
-  else {
-      digitalWrite(LEDlampGreen,  LOW);
-  }
-  
-  if (distance < 20) {
-    digitalWrite(LEDlampBlue,  HIGH);
-}
-  else {
-    digitalWrite(LEDlampBlue,LOW);
-  }
-  if (distance  < 5) {
-    digitalWrite(LEDlampRed, HIGH);
-    sound = 1000;
-}
-  else  {
-    digitalWrite(LEDlampRed,LOW);
-  }
- 
-  if (distanceincm > 5 ||  distanceinsm <= 0){
-    Serial.println("Outside the permissible range of distances");
-    noTone(soundbuzzer);
-  }
-  else {
-    Serial.print(distance);
-    Serial.println("  cm");
-    tone(buzzer, sound);
-  }
-  
-  delay(300);
+  distanceinft = (durationindigit/5) / 29.1;
+
+ if (distanceinft > 10) {
+  Serial.println("We have not detected any cars in your Blind Spots - Change lanes when needed."); 
+ }
+
+ if (distanceinft <= 10 && distanceinft >= 7) { 
+  digitalWrite(LEDlampRed, HIGH); 
+  Serial.println("There is another car within"); 
+  Serial.print(distanceinft); 
+  Serial.println(" feet from your Blind Spot. \nGetting Close, Change Lanes with Caution"); 
+ }
+ else { 
+  digitalWrite(LEDlampRed, LOW); 
+ }
+
+
+  delay(1000);
 }
