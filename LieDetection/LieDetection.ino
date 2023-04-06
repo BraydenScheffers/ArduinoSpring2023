5c7929d2bb81585a63b476b1565809abfc6a66f8
Brayden Scheffers <02brayden.scheffers@gulllakecs.org>
2023-04-06 13:36:02 -0400

    Initial Code
    Not Stable

---
+++ LieDetection/LieDetection.ino
@@ -0,0 +1,54 @@
+/// Programmer: Brayden Scheffers
+/// Date: 5.4.2023
+/// Program: LieDetection 
+
+/// Green, Red, Blue (2,3,4) 
+
+int LEDGreen = 2; 
+int LEDRed = 3; 
+int LEDBlue = 4; 
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LEDGreen, OUTPUT);
+  pinMode(LEDRed,  OUTPUT);
+  pinMode(LEDBlue, OUTPUT);
+  digitalWrite(LEDGreen, HIGH);
+  delay(500);
+  digitalWrite(LEDRed,  HIGH);
+  delay(500);
+  digitalWrite(LEDBlue, HIGH);
+  delay(500);
+}
+
+void  loop()
+{
+  if (analogRead(A0) > 60)
+  {
+    digitalWrite(LEDBlue, HIGH);
+  }
+  else
+  {
+    digitalWrite(LEDBlue,  LOW);
+  }
+  if (analogRead(A0) > 20)
+  {
+    digitalWrite(LEDGreen, HIGH);
+  }
+  else
+  {
+    digitalWrite(LEDGreen,  LOW);
+  }
+  if (analogRead(A0) > 45)
+  {
+    digitalWrite(LEDRed, HIGH);
+  }
+  else
+  {
+    digitalWrite(LEDRed,  LOW);
+  }
+
+  Serial.println(analogRead(A0));
+  delay(20);
+}
